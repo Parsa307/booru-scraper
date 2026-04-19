650c796a75b0b0fe0e4b2f3c1e0089be33fe546d
Parsast <parsastdev@duck.com>
2026-04-19 08:46:57 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## booru-scraper
+
+it scrapes pics at boorus 
+
+## example usage:
+
+```
+scraper.sh date_a_live
+```
---
+++ scraper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# scraper.sh: Scrapes images at yande.re,konachan.com,danbooru.donmai.us
+
+#read -p "Enter the tag: " TAG
+
+YANDERE_URL="https://yande.re"
+KONACHAN_URL="https://konachan.com"
+DANBOORU_API="https://danbooru.donmai.us/posts.json?tags="
+API_QUERY="/post.json?tags="
+
+if [ -z "$1" ]; then
+ # No tag specified.
+  echo "Usage: `basename $0` tag"
+  exit 1
+fi
+
+TAG=$1
+
+#if [ -z "$TAG" ]; then
+ # No tag specified.
+  #echo "A tag is required."
+  #exit 1
+#fi
+
+# Create directory for the images
+mkdir -p "yandere_$TAG"
+mkdir -p "konachan_$TAG"
+mkdir -p "danbooru_$TAG"
+
+# Download the image
+curl -s "${YANDERE_URL}${API_QUERY}${TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "yandere_$TAG" && curl -s "${KONACHAN_URL}${API_QUERY}${TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "konachan_$TAG" && curl -s "${DANBOORU_API}${TAG}" | jq -r '.[].url' | aria2c -i- -d "danbooru_$TAG"
