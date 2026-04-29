9d3544e2baf2ed62e9d05c53a8f26909cba1e560
Parsast <parsastdev@duck.com>
2026-04-29 13:28:25 +0330

    fix danbooru scraping

--- scraper.sh
+++ scraper.sh
@@ -30,4 +30,4 @@ mkdir -p "konachan_$TAG"
 mkdir -p "danbooru_$TAG"
 
 # Download the image
-curl -s "${YANDERE_URL}${API_QUERY}${TAG}${HARDCODED_EXCLUDED_TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "yandere_$TAG" && curl -s "${KONACHAN_URL}${API_QUERY}${TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "konachan_$TAG" && curl -s "${DANBOORU_API}${TAG}" | jq -r '.[].url' | aria2c -i- -d "danbooru_$TAG"
+curl -s "${YANDERE_URL}${API_QUERY}${TAG}${HARDCODED_EXCLUDED_TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "yandere_$TAG" && curl -s "${KONACHAN_URL}${API_QUERY}${TAG}" | jq -r '.[].jpeg_url' | aria2c -i- -d "konachan_$TAG" && curl -s "${DANBOORU_API}${TAG}" | jq -r '.[].file_url' | aria2c -i- -d "danbooru_$TAG"
