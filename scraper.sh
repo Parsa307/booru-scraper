eb549f3d44ad9cbe87c8abd56c6de6314b0285e3
Parsast <parsastdev@duck.com>
2026-07-31 14:38:11 +0330

    fix: multiple tags
    thanks to claude

--- scraper.sh
+++ scraper.sh
@@ -12,7 +12,7 @@ YANDERE_API="https://yande.re/post.json?tags="
 KONACHAN_API="https://konachan.com/post.json?tags="
 DANBOORU_API="https://danbooru.donmai.us/posts.json?tags="
 SAFEBOORU_API="https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1&tags="
-WAIFUIM_API="https://api.waifu.im/images?IsNsfw=All&IncludedTags="
+WAIFUIM_API="https://api.waifu.im/images?IsNsfw=All"
 RULE34_API="https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags="
 RULE34_API_KEY="1b7ac421841879b859d0a0d771b6df8e4a3419e0ce54f234e90bc0c8969ac6ada2972a757a25a606e33dfa9bbd946a0ca231f90c38644336367db73f918b61ed"
 RULE34_USER_ID="5925125"
@@ -28,8 +28,6 @@ TAG=$1
 BOORU=$2
 LIMIT=$3
 
-TAGS=$(echo "$TAG" | tr '+' '_')
-
 # if [ -z "$TAG" ] || [ -z "$BOORU" ]; then
 #  No tag and booru specified.
 #   echo "A tag is required."
@@ -40,32 +38,36 @@ TAGS=$(echo "$TAG" | tr '+' '_')
 case "$BOORU" in
   yandere)
     URL="${YANDERE_API}${TAG}${HARDCODED_EXCLUDED_TAG}"
-    DIR="yandere_$TAGS"
+    DIR="yandere"
     JQ_FILTER='.[].jpeg_url'
     ;;
   konachan)
     URL="${KONACHAN_API}${TAG}"
-    DIR="konachan_$TAGS"
+    DIR="konachan"
     JQ_FILTER='.[].jpeg_url'
     ;;
   danbooru)
     URL="${DANBOORU_API}${TAG}"
-    DIR="danbooru_$TAGS"
+    DIR="danbooru"
     JQ_FILTER='.[].file_url'
     ;;
   safebooru)
     URL="${SAFEBOORU_API}${TAG}"
-    DIR="safebooru_$TAGS"
+    DIR="safebooru"
     JQ_FILTER='.[].file_url'
     ;;
   waifuim)
-    URL="${WAIFUIM_API}${TAG}"
-    DIR="waifuim_$TAGS"
+    URL="${WAIFUIM_API}"
+    IFS='+' read -ra TAG_ARR <<< "$TAG"
+    for tag in "${TAG_ARR[@]}"; do
+      URL="${URL}&IncludedTags=${tag}"
+    done
+    DIR="waifuim"
     JQ_FILTER='.items[].url'
     ;;
   rule34)
     URL="${RULE34_API}${TAG}${HARDCODED_RULE34_EXCLUDED_TAG}&api_key=${RULE34_API_KEY}&user_id=${RULE34_USER_ID}"
-    DIR="rule34_$TAGS"
+    DIR="rule34"
     JQ_FILTER='.[].file_url'
     ;;
   *)
